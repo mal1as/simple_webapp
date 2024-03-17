55ae3e81d6c2ad4a4a36735bf660f043fbe4a100
mal1as <mishadyakonov14050@gmail.com>
2024-03-17 17:25:43 +0300

    Add request param in ControllerAdvice

--- src/main/java/com/mal1as/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/mal1as/exception/ExceptionControllerAdvice.java
@@ -5,25 +5,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler
-    public ResponseEntity<ExtendedResponseDTO> handleValidationException(RequestValidationException e) {
+    public ResponseEntity<ExtendedResponseDTO> handleValidationException(RequestValidationException e, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ExtendedResponseDTO.builder()
                         .response(HttpStatus.BAD_REQUEST.getReasonPhrase() + " : " + e.getMessage())
+                        .request(request.getParameter("key"))
                         .build());
     }
 
     @ExceptionHandler
-    public ResponseEntity<ExtendedResponseDTO> handleInternalServerError(Exception e) {
+    public ResponseEntity<ExtendedResponseDTO> handleInternalServerError(Exception e, HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ExtendedResponseDTO.builder()
                         .response(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase() + " : " + e.getMessage())
+                        .request(request.getParameter("key"))
                         .build());
     }
 }
