bc94b2761e777c4056cfa529268185efca1623be
mal1as <mishadyakonov14050@gmail.com>
2024-03-18 16:13:04 +0300

    Add simple test

---
+++ src/test/java/com/mal1as/SimpleWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mal1as;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
